b7186c768f21aa77e7bbc6be52c2324818962a59
Mirko Brombin <brombinmirko@outlook.it>
2020-10-15 19:12:31 +0200

    Remove build path

--- build/po/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mirko/Projects/rocker-launcher/po
-# Build directory: /home/mirko/Projects/rocker-launcher/build/po
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/Widgets/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mirko/Projects/rocker-launcher/src/Widgets
-# Build directory: /home/mirko/Projects/rocker-launcher/build/src/Widgets
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
